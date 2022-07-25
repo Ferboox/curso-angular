d54364c9c20e264d14164089d06478a5c65f49de
ferbargar99@hotmail.com <ferbargar99@hotmail.com>
2022-07-25 00:33:19 -0500

    Estructura de App, API

---
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+})
+export class PorPaisComponent {
+  termino: string= ''
+
+  constructor(private paisService: PaisService){
+
+  }
+
+  buscar(){
+    this.paisService.buscarPais(this.termino)
+    .subscribe( resp => {
+      console.log(resp);
+    });
+  }
+}
